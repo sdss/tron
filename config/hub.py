e966c740ad85aab8163c7aac62889476c0f234f6
j.sanchezgallego <j.sanchezgallego@D-108-179-131-101.dhcp4.washington.edu>
2015-11-27 15:15:30 +0000

    Remove references to the password file.

--- config/hub.py
+++ config/hub.py
@@ -5,12 +5,5 @@ import os
 # Where to save the logs
 logDir = '/data/logs/tron'
 
-# TODO: this will no longer work, since we're not checking in the password file any more.
-# TODO: consider keyring python module?
-# TODO: see this ticket: https://trac.sdss.org/ticket/2405
-
-# What file has the passwords.
-passwordFile = os.path.join(os.environ['TRON_DIR'], 'passwords')
-
 # Which words to load internally.
 vocabulary = ('perms', 'hub', 'keys', 'msg')
