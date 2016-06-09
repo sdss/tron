d5187fc2bd7bd6903dea410145b77255ff16d845
José Sánchez-Gallego <gallegoj@uw.edu>
2016-06-09 14:35:41 -0600

    updated init commands for TCC

--- Nubs/tcc.py
+++ Nubs/tcc.py
@@ -18,8 +18,15 @@ def start(poller):
     stop()
 
     initCmds = ('show version',
+                'show users',
+                'show time',
                 'show status',
-                )
+                'show inst/full',
+                'show object/full',
+                'show axisconfig',
+                'show focus',
+                'axis status',
+                'mir status')
 
     safeCmds = r"(^show )|(status$)"
 
