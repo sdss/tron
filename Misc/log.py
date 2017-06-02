86c8e1a657fd0e58102b9042a0053a626ccd4d6e
Brian Cherinka <havok2063@hotmail.com>
2017-06-02 09:37:32 -0600

    added local config into tron

--- Misc/log.py
+++ Misc/log.py
@@ -28,24 +28,24 @@ def setID(newID):
     global logID
 
     logID = newID
-    
+
 def setLogdir(dirname):
     global logfileDir
 
     logfileDir = dirname
-    
+
 def enableLoggingFor(system):
     systems[system] = ENABLED
 
 def disableLoggingFor(system):
     systems[system] = DISABLED
-    
+
 def setLoggingFor(system, level):
     if level:
         systems[system] = ENABLED
     else:
         systems[system] = DISABLED
-    
+
 def isoTS(t=None, format="%Y-%m-%d %H:%M:%S", zone="Z"):
     """ Return a proper ISO timestamp for t, or now if t==None. """
 
@@ -54,7 +54,7 @@ def isoTS(t=None, format="%Y-%m-%d %H:%M:%S", zone="Z"):
 
     if zone == None:
         zone = ''
-        
+
     return strftime(format, gmtime(t)) \
            + ".%03d%s" % (1000 * modf(t)[0], zone)
 
@@ -62,7 +62,7 @@ def rollover(t):
     global logfile
     global logfileName
     global rolloverTime
-    
+
     if t > rolloverTime:
         logfile = None
 
@@ -82,15 +82,15 @@ def rollover(t):
             pass
         os.symlink(logfileName, currentName)
         log("log", "next rollover is at %d (%s)" % (rolloverTime, isoTS(rolloverTime)))
-            
+
 def log(system, detail, state=None):
     now = time()
     nowStamp = isoTS(now)
     rollover(now)
-    
+
     #if not hasattr(globals(), 'logfile'):
     #    logfile = sys.stderr
-        
+
     # If the logging state has not explicitely been enabled or disabled,
     # print the notice, but mark the system name with a '?'
     #
@@ -99,11 +99,11 @@ def log(system, detail, state=None):
 
     if state == UNDEFINED:
         state = systems.get('default', state)
-        
+
     if state != DISABLED:
         logfile.write("%s %s %s %s %s\n" % (nowStamp, logID, state, system, detail))
         logfile.flush()
-        
+
 def error(*args):
     apply(log, args, {'state':ERROR})
-    
+
