48a074a7c34e59198aae32f57bfe082802a6d007
José Sánchez-Gallego <gallegoj@uw.edu>
2020-01-02 18:06:38 -0800

    Rotating Nub logging

--- Hub/Nub/CoreNub.py
+++ Hub/Nub/CoreNub.py
@@ -10,15 +10,15 @@ class CoreNub(IO.IOHandler):
 
     Because of the asymmetric nature of the protocol, each Nub must subclass from
     either CommanderNub or ActorNub, and not from CoreNub directly.
-    
+
     - sendCommand(cmd)
     - sendReply(reply)
     - parseCommand(input)
     - parseReply(input)
-    
+
     Each connection can/must be configured with specific:
        - input decoder  -- recognizes and extracts complete input chunks.
-       - output encoder -- 
+       - output encoder --
     """
 
     def __init__(self, poller, **argv):
@@ -28,14 +28,14 @@ class CoreNub(IO.IOHandler):
         self.nubType = argv.get('type', 'unknown')
         self.needsAuth = argv.get('needsAuth', False)
         self.userInfo = None
-        
+
         IO.IOHandler.__init__(self, poller, **argv)
 
         self.encoder = argv.get('encoder', None)
         self.decoder = argv.get('decoder', None)
         self.encoder.setNub(self.ID)
         self.decoder.setNub(self.ID)
-        
+
         self.otherIP = argv.get('otherIP', None)
         self.otherFQDN = argv.get('otherFQDN', None)
 
@@ -44,13 +44,13 @@ class CoreNub(IO.IOHandler):
 
         logDir = argv.get("logDir", None)
         if logDir:
-            self.log = Misc.Logfile(logDir, EOL='', doEncode=True)
+            self.log = Misc.Logfile('nub.' + self.name, logDir)
         else:
             self.log = None
-            
+
     def __str__(self):
         return "CoreNub(id=%s, name=%s, type=%s)" % (self.ID, self.name, self.nubType)
-    
+
     def setName(self, newName):
         """ Change our username(s). """
 
@@ -60,25 +60,25 @@ class CoreNub(IO.IOHandler):
 
     def connected(self):
         pass
-    
+
     def shutdown(self, **argv):
-        """ Release all resources and shut down. 
+        """ Release all resources and shut down.
 
         Args:
            notifyHub(True)     - call the hub rather than clean up ourselves.
            why('')             - i
-           
+
         If called from "below" (i.e. a socket has been shutdown), just
         calls the hub's dropNub method, which will shortly call us back.
-        
+
         If called from the hub, close all IO resources.
         """
 
         notifyHub = argv.get('notifyHub', True)
         why = argv.get('why', '')
-        
+
         Misc.log("Hub.shutdown", "notify=%s why=%s" % (notifyHub, why))
-        
+
         if notifyHub:
             hub.dropNub(self)
         else:
@@ -86,7 +86,7 @@ class CoreNub(IO.IOHandler):
 
     def flagFinishesCommand(self, f):
         """ Return True if a reply flag completes the command. """
-        
+
         return f in ':fF'
 
     def statusCmd(self, cmd, doFinish=True):
@@ -97,5 +97,3 @@ class CoreNub(IO.IOHandler):
 
         if doFinish:
             cmd.finish()
-            
-    
