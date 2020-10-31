2024319bb3677bd5d141f5aba18b2c60867e16c1
José Sánchez-Gallego <gallegoj@uw.edu>
2020-10-31 09:33:16 -0700

    Tweak build process and dependencies

--- tron/__init__.py
+++ tron/__init__.py
@@ -1,30 +1,6 @@
-import configparser
-import os
+from sdsstools import get_package_version
 
 
-try:
-    import pkg_resources
-except (ImportError, ModuleNotFoundError):
-    pkg_resources = None
+NAME = 'sdss-tron'
 
-
-def get_version():
-
-    if pkg_resources:
-        try:
-            return pkg_resources.get_distribution('sdss-tron').version
-        except pkg_resources.DistributionNotFound:
-            pass
-
-    setup_cfg = os.path.join(os.path.dirname(__file__), '../setup.cfg')
-    if os.path.exists(setup_cfg):
-        config = configparser.ConfigParser()
-        config.read(setup_cfg)
-        return config.get('metadata', 'version')
-
-    return 'dev'
-
-
-__version__ = get_version()
-
-NAME = 'tron'
+__version__ = get_package_version(path='./', package_name=NAME)
