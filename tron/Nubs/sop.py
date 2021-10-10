e35a4bd0793bb6545eefbcae894669a485ad9b57
José Sánchez-Gallego <gallegoj@uw.edu>
2021-10-10 11:59:45 -0700

    Add SOP alias for HAL

---
+++ tron/Nubs/sop.py
@@ -0,0 +1,45 @@
+import os.path
+
+import tron.Misc
+from tron import g, hub
+from tron.Hub.Command.Encoders.ASCIICmdEncoder import ASCIICmdEncoder
+from tron.Hub.Nub.SocketActorNub import SocketActorNub
+from tron.Hub.Reply.Decoders.ASCIIReplyDecoder import ASCIIReplyDecoder
+
+
+name = 'sop'
+
+
+def start(poller):
+    cfg = tron.Misc.cfg.get(g.location, 'actors', doFlush=True)['hal']
+
+    stop()
+
+    initCmds = ('ping', 'status', 'version')
+
+    safeCmdsList = ['ping', 'version', 'status']
+    safeCmds = r'^\s*({0})\s*$'.format('|'.join(safeCmdsList))
+
+    d = ASCIIReplyDecoder(cidFirst=True, debug=1)
+    e = ASCIICmdEncoder(sendCommander=False, useCID=False, debug=1)
+    nub = SocketActorNub(
+        poller,
+        cfg['host'],
+        cfg['port'],
+        name=name,
+        encoder=e,
+        decoder=d,
+        grabCID=True,  # the actor spontaneously generates a line we can eat.
+        initCmds=initCmds,
+        safeCmds=safeCmds,
+        needsAuth=True,
+        logDir=os.path.join(g.logDir, name),
+        debug=3)
+    hub.addActor(nub)
+
+
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
