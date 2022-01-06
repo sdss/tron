3c0a9b6a65a1187331015a972b949e3c345e4361
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-06 18:42:53 +0000

    Add apogeefpi nub

---
+++ tron/Nubs/apogeefpi.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-01-06
+# @Filename: apogeefpi.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import os
+
+import tron.Misc
+from tron import g, hub
+from tron.Hub.Command.Encoders.ASCIICmdEncoder import ASCIICmdEncoder
+from tron.Hub.Nub.SocketActorNub import SocketActorNub
+from tron.Hub.Reply.Decoders.ASCIIReplyDecoder import ASCIIReplyDecoder
+
+
+name = 'apogeefpi'
+
+
+def start(poller):
+    cfg = tron.Misc.cfg.get(g.location, 'actors', doFlush=True)[name]
+
+    stop()
+
+    initCmds = ('ping', 'version')
+
+    safeCmdsList = ['ping', 'version', 'status']
+    safeCmds = r'^\s*({0})\s*$'.format('|'.join(safeCmdsList))
+
+    d = ASCIIReplyDecoder(cidFirst=True, debug=1)
+    e = ASCIICmdEncoder(sendCommander=True, useCID=False, debug=1)
+    nub = SocketActorNub(
+        poller,
+        cfg['host'],
+        cfg['port'],
+        name=name,
+        encoder=e,
+        decoder=d,
+        grabCID=True,  # the actor spontaneously generates a line we can eat.
+        initCmds=initCmds,
+        safeCmds=safeCmds,
+        needsAuth=True,
+        logDir=os.path.join(g.logDir, name),
+        debug=3)
+    hub.addActor(nub)
+
+
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
